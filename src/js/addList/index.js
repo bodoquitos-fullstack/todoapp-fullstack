438af596be1ff5d82c44bb397b4ca4f8dccdfb3d
Jonh <jonathan@laboratoria.la>
2018-03-21 14:55:47 -0600

    cambios pequeños en estilos

--- src/js/addList/index.js
+++ src/js/addList/index.js
@@ -1,13 +1,3 @@
-//   <!-- <div class="col s12 m5 todo">
-//   <div class="card-panel list">
-//   Lista 1.
-// </label>
-// <a class="btn-floating btn-large waves-effect waves-light red" ><i class="material-icons">add</i></a>
-// <i class="medium material-icons">delete</i>   
-// </div>
-// </div>
-
-
 const addList = {
   init() {
     $('#todo-form').submit(addList.addTodoList);
@@ -15,20 +5,23 @@ const addList = {
   addTodoList(e) {
     e.preventDefault();
     const { value: list} = document.getElementById('list');
-    console.log('pruena');
+    const titulo = $('#list').val();
+    
 
-    let todoId = `list-${addList.counter + 1}`;
+    let todoId = `list-${addList.counter + 1}`; 
     const plantilla =
     `<div class="col s12 m5 list">
         <div class="card-panel list">
-          Lista 1.
-          <a class="btn-floating btn-large waves-effect waves-light red" ><i class="material-icons">add</i></a>
-          <i class="medium material-icons">delete</i>   
+          ${titulo}
+          <hr>
+          <a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+          <a href=""><i class="medium material-icons">delete</i></a>
         </div>
     </div>`;
 
     $('#todos').append(plantilla);
-  },
+    $('#list').val("");
+    },
   counter: 0
 }
 
