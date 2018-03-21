05ace5528b81a3f97738d0fc7e4bbbf57bbde0f6
Jonh <jonathan@laboratoria.la>
2018-03-21 13:30:55 -0600

    creando listas dinamicas

--- src/js/addTask/index.js
+++ src/js/addTask/index.js
@@ -1,27 +1,27 @@
-const addTask = {
-    init() {
-        $('#todo-form').submit(addTask.addTodo);
-    },
-    addTodo(event) {
-        event.preventDefault();
-        const { value: todo } = document.getElementById('todo');
+// const addTask = {
+//     init() {
+//         $('#todo-form').submit(addTask.addTodo);
+//     },
+//     addTodo(event) {
+//         event.preventDefault();
+//         const { value: todo } = document.getElementById('todo');
 
-        let todoId = `todo-${addTask.counter + 1}`;
+//         let todoId = `todo-${addTask.counter + 1}`;
 
-        const $todoContainer = $('<div />').addClass('col s12 m6 todo');
-        const $todoCard = $('<div />').addClass('card-panel');
-        const $todoCheckbox = $('<input type="checkbox" />').attr('id', todoId);
-        const $todoText = $('<label />').attr('for', todoId).text(todo);
+//         const $todoContainer = $('<div />').addClass('col s12 m6 todo');
+//         const $todoCard = $('<div />').addClass('card-panel');
+//         const $todoCheckbox = $('<input type="checkbox" />').attr('id', todoId);
+//         const $todoText = $('<label />').attr('for', todoId).text(todo);
 
-        $todoCard.append($todoCheckbox);
-        $todoCard.append($todoText);
-        $todoContainer.append($todoCard);
+//         $todoCard.append($todoCheckbox);
+//         $todoCard.append($todoText);
+//         $todoContainer.append($todoCard);
 
-        $('#todos').append($todoContainer);
+//         $('#todos').append($todoContainer);
 
-        app.counter = app.counter + 1;
-    },
-    counter: 0
-};
+//         app.counter = app.counter + 1;
+//     },
+//     counter: 0
+// };
 
-module.exports = app;
+// module.exports = app;
