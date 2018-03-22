ecfb7f5eb5beeb4ff82a8383412be81a1e1d99f2
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2018-03-22 17:31:15 -0600

    funcion delate

--- src/js/addList/index.js
+++ src/js/addList/index.js
@@ -1,4 +1,6 @@
-var task = require('../addTask');
+const task = require('../addTask');
+const delate = require('../remove');
+
 const list = {
   init() {
     task.init();
@@ -18,13 +20,14 @@ const list = {
               <h1>${title}</h1>
               <hr>
               <a class="small waves-effect waves-light btn right form-tasks">Add</a>
-              <a href="#" class="right"><i class="small material-icons">delete</i></a>
+              <a id="delate" class="right"><i class="small material-icons">delete</i></a>
               <div id="${containerTasksId}" class="row"></div>
             </div>
         </div>`;
 
     $('#todos').prepend(plantilla);
     $('.form-tasks').click(task.formTasks);
+    $('#delate').click(delate);
     list.counter = list.counter + 1;
     $("#list").val("");
   },
